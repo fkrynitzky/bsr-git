f4752a056c53340c3873d6c1546843f70d5537e0
romanCodingMachine <46537318+romanCodingMachine@users.noreply.github.com>
2019-01-10 12:50:15 +0100

    Update Lunar Arithmetics.cpp

--- Lunar Arithmetics.cpp
+++ Lunar Arithmetics.cpp
@@ -53,7 +53,13 @@ string lunarMultiplication(string x, string y){
 			}
 		}
 	}
-	number[1] += '0';
+	
+	//Adding zeros to the end of the strings respectfully
+	for(int i = 1; i < x.length(); i++){
+		for(int k = 0; i < x.length(); i++){
+			number[i] += '0';
+		}
+	}
 	
 	return lunarAddition(number[0],number[1]);
 }
