d4eb023c0a4151e3889017615267d6671f75def9
AntekB <antek.cood.12@gmail.com>
2019-01-10 18:51:56 +0100

    Add lunar multiplication, plus create C++ folder

--- python3/lunar_arthmetic.py
+++ python3/lunar_arthmetic.py
@@ -1,29 +1,39 @@
 
-def adding_beta(x,y):
+def adding(x,y):
+    if(len(y) > len(x)):
+        x,y = y,x
     anwser = ""
     reverse_x = x[::-1]
     reverse_y = y[::-1]
-    if(len(x) > len(y)):
-        for z in range(0,len(y)):
-            if(int(reverse_x[z]) > int(reverse_y[z])):
-                anwser += reverse_x[z]
-            else: anwser += reverse_y[z]
-        for z in range(len(y),len(x)):
+    for z in range(0,len(y)):
+        if(int(reverse_x[z]) > int(reverse_y[z])):
             anwser += reverse_x[z]
-    else: 
-        for z in range(0,len(x)):
-            if(int(reverse_x[z]) > int(reverse_y[z])):
-                anwser += reverse_x[z]
-            else: anwser.append(reverse_y[z])
-        for z in range(len(x),len(y)):
-            anwser += reverse_y[z]
-                   
+        else: anwser += reverse_y[z]
+    for z in range(len(y),len(x)):
+        anwser += reverse_x[z]            
     return ''.join(reversed(anwser))
 
-
-
 def multiplication(x,y):
-                
+    if(len(y) > len(x)):
+        x,y = y,x
+    sum1 = ""
+    sum2 = ""        
+    reverse_x = x[::-1]
+    reverse_y = y[::-1]
+    for z in range(0,len(y)):
+        for t in range(0,len(x)):
+            if(int(reverse_y[z]) < int(reverse_x[t])):
+               sum1 += reverse_y[z]
+            else: sum1 += reverse_x[t]
+        sum1 = sum1[::-1]
+        u = z
+        while(u>0):
+            sum1 += "0"
+            u -= 1
+        sum2 = adding_beta(sum1,sum2)
+        sum1 = ""
+    return ''.join(sum2)
+
     
 if(__name__ == "__main__"):
     while True:
@@ -32,9 +42,9 @@ if(__name__ == "__main__"):
         number_b = input("Please wrtie second number: ")
         choice = input("Do you want to multiply or add. [m/a]  ")
         if (choice == 'm'):
-            print("This anwser is not ready yet. Please try later.")
+            print(f"This anwser is {multiplication(number_a,number_b)}")
         elif (choice == 'a'):
-            print(f"The answer is {adding_beta(number_a,number_b)}")
+            print(f"The answer is {adding(number_a,number_b)}")
         else: print("Wrong anwser please try again")
 
         
